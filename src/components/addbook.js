417ad901ca93ea3e6860ecc27c52e41454d4220f
Myra James <Treesmellgood@gmail.com>
2020-12-09 20:32:02 -0500

    initial commit

---
+++ src/components/addbook.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+
+export default function addBook(props) {
+    const [title, setTitle] = useState("")
+    const [author, setAuthor] = useState("")
+    const [review, setReview] = useState("")
+
+    const handleSubmit = event => {
+        event.prevent.default()
+        //prevents onSubmits default settings of refreshing page
+    if (title !== "" && author !== "") {
+        fetch("http://127.0.0.1:5000/book/add", {
+            method="POST",
+            headers: {"content-type": "application/json"},
+            body: JSON.stringify({
+                title:title,
+                author: author,
+                review: review
+            })
+
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    }
+    }
+    return (
+       <div className='add-book-wrapper'>
+          <form onSubmit={handleSubmit}>
+            <input 
+              type="text"
+              value={title}
+              placeholder="Title"
+              onChange={event => setTitle(event.target.value)}
+            />
+
+             <input  
+              type="author"
+              value={author}
+              placeholder="Author"
+              onChange={event => setAuthor(event.target.value)}
+            />
+             <textarea
+              value={review}
+              placeholder="Review"
+              onChange={event => setReview(event.target.value)}
+            />
+
+            <button type="submit">Add Book</button>
+
+          </form>
+       </div>
+    )
+}
